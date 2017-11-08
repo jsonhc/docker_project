3992ca5b733b7c64070db12457e5e6819352b48c
wadeson <json_hc@163.com>
2017-11-08 23:49:05 +0800

    Delete mariadb.sql

--- docker_dockerfile/lnmp/mysql/mariadb.sql
+++
@@ -1,8 +0,0 @@
-create database wordpress DEFAULT CHARACTER SET utf8;
-
-USE mysql;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
-UPDATE user SET password=PASSWORD("root") WHERE user='root';
-FLUSH PRIVILEGES;
