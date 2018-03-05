57ba6afd26437501cdf7afaf4ec8f0a0c00cd01f
wadeson <json_hc@163.com>
2018-03-05 12:32:35 +0800

    Update business.sql

--- docker_dockerfile/mysql/business.sql
+++ docker_dockerfile/mysql/business.sql
@@ -8,3 +8,15 @@ FLUSH PRIVILEGES;
 
 UPDATE user SET password=PASSWORD("root") WHERE user='root';
 FLUSH PRIVILEGES;
+
+
+
+或者：
+grant all privileges on *.* to 'root'@'localhost' identified by 'redhat';
+
+USE mysql;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' identified by 'redhat';
+FLUSH PRIVILEGES;
+
+UPDATE user SET password=PASSWORD("redhat") WHERE user='root';
+FLUSH PRIVILEGES;
